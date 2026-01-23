05c77c73dcd9b94e5c7617bf9b90b2ebed461adb
Th3R3ality <63677818+Th3R3ality@users.noreply.github.com>
2026-01-23 05:50:21 +0100

    separate gui to its own file. add fuzzy-matcher crate for future. renamed controls::CONTROLS -> controls::INPUT_CODES

---
+++ src/gui.rs
@@ -0,0 +1,106 @@
+use crate::controls;
+
+use iced::{
+    Element, Size, Task, Theme, alignment, widget::{
+        button, column, container, row, scrollable, stack, text
+    }
+};
+
+pub fn run() -> iced::Result {
+    iced::application(|| (Counter::default(), Task::none()), update, view)
+        .title(Counter::title)
+        .theme(Theme::Dark)
+        .antialiasing(true)
+        .run()
+}
+
+#[derive(Default)]
+struct Counter {
+    value: i64,
+}
+
+impl Counter {
+    fn title(&self) -> String {
+        format!("Counter: {}", self.value)
+    }
+}
+
+#[derive(Debug, Clone)]
+enum Message {
+    Increment,
+    Decrement,
+    Reset,
+}
+
+fn update(counter: &mut Counter, message: Message) -> Task<Message> {
+    match message {
+        Message::Increment => {
+            counter.value += 1;
+        }
+        Message::Decrement => {
+            counter.value -= 1;
+        }
+        Message::Reset => {
+            counter.value = 0;
+        }
+    }
+    Task::none()
+}
+
+static mut bloopy: usize = 0;
+fn view(counter: &Counter) -> Element<Message> {
+    let display = text(counter.value.to_string())
+        .size(42)
+        .style(|_| text::Style {
+            color: Some(iced::Color::from_rgb8(220, 220, 255)),
+            ..Default::default()
+        }
+    );
+
+    let control: &str;
+
+    unsafe {
+        control = controls::INPUT_CODES.get(bloopy)
+        .copied()
+        .unwrap_or("FUCK");
+        
+        bloopy += 1; bloopy %= controls::INPUT_CODES.len();
+    }
+
+    
+
+    let background: Element<Message> = scrollable(
+        column![].extend(controls::INPUT_CODES.iter().map(|line| text(*line).into())),
+    )
+    .width(iced::Length::Fill)
+    .height(iced::Length::Fill)
+    .into();
+
+    let main_content =
+        column![
+            text("Counter Example")
+                .size(32)
+                .width(iced::Length::Fill)
+                .align_x(iced::alignment::Horizontal::Center),
+
+            row![
+                button("-10").on_press(Message::Decrement),
+                button("-1") .on_press(Message::Decrement),
+                display,
+                button("+1") .on_press(Message::Increment),
+                button("+10").on_press(Message::Increment),
+            ]
+            .spacing(12)
+            .align_y(iced::Alignment::Center),
+
+            text(control),
+            button("Reset")
+                .on_press(Message::Reset)
+                .padding([12, 32]),
+        ]
+        .padding(40)
+        .spacing(32)
+        .align_x(iced::Alignment::Center);
+
+    stack!(background, main_content).into()
+}
--- src/main.rs
+++ src/main.rs
@@ -1,49 +1,8 @@
-#[derive(Default)]
-struct Counter {
-    value: i32,
-}
-#[derive(Debug, Clone, Copy)]
-pub enum Message {
-    Increment,
-    Decrement,
-}
+mod controls;
+mod gui;
 
-use iced::padding;
-use iced::widget::{button, column, text, Column, container};
-impl Counter {
-    pub fn view(&self) -> Column<Message> {
-        // We use a column: a simple vertical layout
-        column![
-            // The increment button. We tell it to produce an
-            // `Increment` message when pressed
-            button("+").on_press(Message::Increment),
 
-            // We show the value of the counter here
-            text(self.value).size(50),
-            container("Independent padding!")
-                .padding(padding::vertical(30).left(20).right(80))
-                .style(container::bordered_box),
-            
+pub fn main() {
 
-            // The decrement button. We tell it to produce a
-            // `Decrement` message when pressed
-            button("-").on_press(Message::Decrement),
-        ]
-    }
-
-    pub fn update(&mut self, message: Message) {
-        match message {
-            Message::Increment => {
-                self.value += 1;
-            }
-            Message::Decrement => {
-                self.value -= 1;
-            }
-        }
-    }
-}
-
-fn main() {
-    println!("Hello, world!");
-    iced::run(Counter::update, Counter::view);
+    gui::run();
 }
